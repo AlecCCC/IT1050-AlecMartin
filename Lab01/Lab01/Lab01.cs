527f7d9b27dc0fb3315205bc5a06ad2ea19142e7
Alec 404 <31903030+AlecM96@users.noreply.github.com>
2017-09-17 16:32:52 -0400

    Lab01

---
+++ Lab01/Lab01/Lab01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Lab01
+    {
+        static void Main(string[] args)
+        {
+            String person = "Alec";
+            Console.WriteLine($"Hello World from {person}");
+        }
+    }
+}
