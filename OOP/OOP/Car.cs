7effca2db7761f6c6ad54648e03e993d019783a0
Alec 404 <31903030+AlecM96@users.noreply.github.com>
2017-12-11 22:41:34 -0500

    Stuck at 5

---
+++ OOP/OOP/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Car
+    {
+        private int Speed;
+        private string Make;
+        private string Model;
+        private int Year;
+
+        public Car()
+        {
+        }
+
+        public Car(string make, string model, int year, int speed)
+        {
+                this.Make = make;
+                this.Model = model;
+                this.Year = year;
+                this.Speed = speed;
+        }
+
+        public Car(string make, string model, int year)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            Speed = 0;
+        }
+
+      public void SpeedUp(int speedIncrement)
+        {
+
+        }
+    
+      public void SlowDown(int speedDecrement)
+        {
+
+        }
+    }
+}
---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int car1Speed = 20;
+            int car2Speed = 0;
+
+            Car car1 = new Car("Ford", "Focus", 2018, car1Speed);
+            Car car2 = new Car("Subaru", "WRX", car2Speed);
+
+
+
+        }
+    }
+}
